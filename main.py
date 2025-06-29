cfd81549b19db5ae485bcad74f1be8812e4fcc26
tozalas <thomas.ozalas@gmail.com>
2025-06-28 21:37:04 -0400

    6: draw player on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 import constants
+import player
 
 
 def main():
@@ -11,12 +12,14 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    user = player.player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             screen.fill((0,0,0))  # Fill the screen with the background color
+            user.draw(screen)
             pygame.display.flip()  # Update the display
             dt = clock.tick(60) / 1000
 
