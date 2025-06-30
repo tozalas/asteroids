3e8e68a26bc5d554750a21e41b18b279feba9f55
tozalas <thomas.ozalas@gmail.com>
2025-06-29 20:16:32 -0400

    9: added sprite groups

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-import constants
-import player
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
-    user = player.player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            user.update(dt)
-            screen.fill((0,0,0))  # Fill the screen with the background color
-            user.draw(screen)
+        updatable.update(dt)
 
-            pygame.display.flip()  # Update the display
+        screen.fill("black")
 
-            dt = clock.tick(60) / 1000
-    
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {constants.SCREEN_WIDTH}")
-    # print(f"Screen height: {constants.SCREEN_HEIGHT}")
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
